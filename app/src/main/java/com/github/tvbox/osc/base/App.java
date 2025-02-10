e15b72078e9060112e9be34a52dedc34c73de062
zhixc <>
2025-02-11 00:15:52 +0800

    关于信息修改，默认app设置调整

--- app/src/main/java/com/github/tvbox/osc/base/App.java
+++ app/src/main/java/com/github/tvbox/osc/base/App.java
@@ -47,9 +47,23 @@ public class App extends MultiDexApplication {
         // Hawk
         Hawk.init(this).build();
         Hawk.put(HawkConfig.DEBUG_OPEN, false);
-        if (!Hawk.contains(HawkConfig.PLAY_TYPE)) {
-            Hawk.put(HawkConfig.PLAY_TYPE, 2);
-        }
+
+        // 首页选项
+        putDefault(HawkConfig.HOME_REC, 2);                  //推荐: 0=豆瓣热播, 1=站点推荐, 2=观看历史
+        // 播放器选项
+        putDefault(HawkConfig.PLAY_SCALE, 0);                //画面缩放: 0=默认, 1=16:9, 2=4:3, 3=填充, 4=原始, 5=裁剪
+        putDefault(HawkConfig.PLAY_TYPE, 2);                 //播放器: 0=系统, 2=Exo
+        // 系统选项
+        putDefault(HawkConfig.SEARCH_VIEW, 0);               //搜索展示: 0=文字列表, 1=缩略图
+        putDefault(HawkConfig.PARSE_WEBVIEW, true);          //嗅探Webview: true=系统自带, false=XWalkView
+        putDefault(HawkConfig.DOH_URL, 0);                   //安全DNS: 0=关闭, 1=腾讯, 2=阿里, 3=360
+
+        // 渲染方式
+        putDefault(HawkConfig.PLAY_RENDER, 1);               //渲染方式 0=TextureView, 1=SurfaceView
+    }
+
+    private void putDefault(String key, Object value) {
+        if (!Hawk.contains(key)) Hawk.put(key, value);
     }
 
     public static App getInstance() {
